2f40f74e97e06bf8681b220e349e469ae10fd6c5
arijitdas122001 <aribwnlvpgbwn@gmail.com>
2023-03-25 02:23:05 +0530

    dsu

--- dsu.cpp
+++ dsu.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 class DisjointSet
 {
+   vector<int> rank, parent,size;
 public: 
-   vector<int> rank, parent;
     DisjointSet(int n)
     {
         rank.resize(n+1, 0);
         parent.resize(n + 1);
+        size.resize(n+1,0);
         for (int i = 0; i <=n; i++)
         {
             parent[i] = i;
@@ -40,18 +41,36 @@ public:
             rank[up]++;
         }
     }
+    void unionBysize(int u, int v)
+    {
+        int up = findparent(u);
+        int uv = findparent(v);
+        if(size[up]>size[uv]){
+            parent[uv]=up;
+            size[up]+=size[uv];
+        }
+        else{
+            parent[up]=uv;
+            size[uv]+=size[up];
+        }
+    }
 };
 int main()
 {
     DisjointSet ds(7);
-    ds.unionByrank(1, 2);
-    ds.unionByrank(2, 3);
-    ds.unionByrank(4, 5);
-    ds.unionByrank(6, 7);
-    ds.unionByrank(5, 6);
-    ds.unionByrank(3,7);
+    // ds.unionBysize(1, 2);
+    // ds.unionBysize(2, 3);
+    // ds.unionBysize(4, 5);
+    // ds.unionBysize(6, 7);
+    // ds.unionBysize(5, 6);
+    // ds.unionBysize(3,7);
+    ds.unionBysize(1, 2);
+    ds.unionBysize(2, 3);
+    ds.unionBysize(4, 5);
+    ds.unionBysize(6, 7);
+    ds.unionBysize(5, 6);
     if(ds.findparent(3)==ds.findparent(7))cout<<"Same\n";
     else cout<<"Not Same\n";
-
+    ds.unionBysize(3,7);
     return 0;
 }
